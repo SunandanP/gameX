f346776820fbc2b6caedbc2ab7fae96100afc06e
SunandanP <sunandanphalke@gmail.com>
2023-06-13 19:24:11 +0530

    Implemented useGames hook

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: String;
-}
-
-interface Responce {
-  count: number;
-  results: Game[];
-}
+import useGames from "../Hooks/useGames";
 
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<Responce>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-        console.log(res.data.results);
-      })
-      .catch((err) => setError(err.message));
-  }, []);
-
+  const { games, error } = useGames();
   return (
     <>
-      {error && <p>{error}</p>}
+      {error != "canceled" && error && <p>{error}</p>}
       <ul>
         {games.map((game) => (
           <li key={game.id}>{game.name}</li>
