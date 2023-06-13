f8f2d368f2907e63106cf695edcff86e798ae024
SunandanP <sunandanphalke@gmail.com>
2023-06-13 17:33:09 +0530

    Layed out Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./Components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button colorScheme="purple">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"}>
+          <Navbar />
+        </GridItem>
+        <GridItem area={"main"} bg={"blue"}>
+          main
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"black"}>
+            aside
+          </GridItem>
+        </Show>
+      </Grid>
     </>
   );
 }
